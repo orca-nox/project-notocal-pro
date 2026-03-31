307aec963f4dc7729c9b207e64545b81f3a10dab
Nox <orca.nocturnalis@gmail.com>
2026-03-31 17:13:40 +0900

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-zinc-950 text-zinc-100">
+      <h1 className="text-4xl font-bold">Notocal Pro</h1>
+    </div>
+  )
+}
+
+export default App
